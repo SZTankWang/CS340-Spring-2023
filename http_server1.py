fd3956d77cbc84010cdf3ff90ae88813eee8c149
zhenming wang <zhenmingwang@dhcp-10-105-115-137.wireless.northwestern.private>
2023-01-19 21:35:24 -0600

    project1/3

---
+++ http_server1.py
@@ -0,0 +1,68 @@
+import os, socket, sys
+
+def get_path(data):
+    index = data.find("\r\n")
+    if index == -1:
+        return ""
+    first_line = data[:index]
+    arrs = first_line.split()
+    if len(arrs) != 3:
+        return ""
+    path = arrs[1]
+    return path
+
+def check_requested_file(path):
+    if path == '/':
+        return -1
+    else:
+        requested_file = path[1:]
+        if os.path.exists(requested_file):
+            tmp = requested_file.split('.')
+            if len(tmp) > 1:
+                if tmp[-1] == 'htm' or tmp[-1] == 'html':
+                    return 200
+                else:
+                    return 403
+            else:
+                return 404
+
+def construct_response(status, content):
+    head_string = "HTTP/1.1 %s \r\nServer: simple server \r\n"\
+        "Content-Type: text/html; charset=utf-8\r\n"\
+            "Content-Length: %d\r\nConnection: close\r\n\r\n"%(status, len(content.encode('utf-8')))
+    return head_string + content
+
+def http_server(SERVER_HOST, SERVER_PORT):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    server_socket.listen(1)
+    print('Listening on port %s ...' % SERVER_PORT)
+
+    while True:
+        client_connection, client_address = server_socket.accept()
+        request = client_connection.recv(1024).decode()
+        path = get_path(request)
+
+        exist_flag = check_requested_file(path)
+        print("exist flag",exist_flag)
+        if exist_flag == 200:
+            fin = open(path[1:])
+            content = fin.read()
+            fin.close()
+            message = construct_response('200 OK', content)
+        if exist_flag == 404:
+            message = 'HTTP/1.0 404 Not Found\n\nFile Not Found'
+        if exist_flag == 403:
+            message = 'HTTP/1.0 403 Forbidden\n\nForbidden'
+
+        client_connection.sendall(message.encode())
+        client_connection.close()
+
+    server_socket.close()
+
+if __name__ == '__main__':
+    try:
+        http_server('0.0.0.0', int(sys.argv[1]))
+    except Exception as e:
+        print(e)
---
+++ http_server2.py
@@ -0,0 +1,105 @@
+import socket
+import sys
+import select
+import urllib3
+import os
+import signal
+import re
+
+def send_data(socket,data,connections):
+    #see the request method
+    data = data.decode("utf-8").split("\r\n")
+    request_info = data[0].split(" ")
+
+    method = request_info[0]
+
+    proto = "HTTP/1.1"
+
+    response_header = {
+        'Content-Type': 'text/html; encoding=utf8',
+        "Connection":"close"
+    }
+    if method == "GET":
+        file = None
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        #ready to serve content
+        try:
+            file = dir_path+request_info[1]
+        except:
+            print("no file requested")
+        print("file is",file)
+        valid_type = file.endswith("html") or file.endswith("htm")
+        if file and os.path.isfile(file) and valid_type:
+            status = "200"
+            response_text = "OK"
+            f = open(file,"r")
+            text_body = f.read()
+            response_header["Content-Length"] = len(text_body)
+            response_header = "".join("%s:%s\n"% (key,value) for (key,value) in response_header.items())
+            socket.send(str.encode("%s %s %s\n" % (proto,status,response_text),"utf-8"))
+            socket.send(str.encode(response_header,"utf-8"))
+            socket.send(str.encode("\n"))
+            socket.send(str.encode(text_body,"utf-8"))
+            print("finished, closing socket")
+            socket.close()
+            connections.remove(socket)
+        else:
+            if not os.path.isfile(file):
+                status = "404"
+                response_text = "NOT FOUND"
+            if not valid_type:
+                status = "403"
+                response_text = "FORBIDDEN"
+            socket.send(str.encode("%s %s %s" % (proto, status, response_text), "utf-8"))
+            response_header["Content-Length"] = 0
+            response_header = "".join("%s:%s\n" % (key, value) for (key, value) in response_header.items())
+            socket.send(str.encode(response_header, "utf-8"))
+            socket.send(str.encode("\n"))
+            socket.close()
+            connections.remove(socket)
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    host = ""
+    connections = []
+    outputs = []
+
+
+    #create server socket
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #add server to the conneciton list
+    connections.append(s)
+
+    #listen to the designated port and all IP
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind((host,port))
+    s.listen(5)
+
+    print("server begins to listen on ",host,port)
+    while len(connections) > 0:
+        readable, writable, exceptional = select.select(connections,
+                                                        outputs,
+                                                        connections)
+        for i in readable:
+            #if s is server, that means we have an incoming connection
+            if i is s:
+                conn,addr = i.accept()
+                print("connection from ",addr)
+                conn.setblocking(0)
+                #add this new socket to connections
+                connections.append(conn)
+            else:
+                #prepare to send data
+                data = i.recv(1024)
+                if data:
+                    print("data received from ",i.getpeername())
+                    # print(data)
+                # parse data
+                    send_data(i,data,connections)
+
+                else:
+                    #data is empty, remove it from connections
+                    print("closing ",i.getpeername)
+                    connections.remove(i)
+                    i.close()
